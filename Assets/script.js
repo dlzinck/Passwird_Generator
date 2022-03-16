b62b03a1b4f9c8e9f8ae3e695080442e61d8596c
Drew Zinck <dlzincktheii@gmail.com>
2022-03-15 17:01:56 -0700

    added negative, one positive, and two positive selections

--- Assets/script.js
+++ Assets/script.js
@@ -13,7 +13,7 @@ let confirmLowercase;
 special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "?", "|"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space = [ ];
+space = [];
 
 let choices;
 
@@ -24,7 +24,7 @@ let upperCase = function (x) {
 };
 
 //Variable for uppercase change
-letterOne = letter.map(upperCase);
+letterUpper = letter.map(upperCase);
 
 
 let get = document.querySelector("#generate");
@@ -36,24 +36,59 @@ get.addEventListener("click", function () {
 
 
 //Generate password function
-function generatePassword() {}
+function generatePassword() {
+  enter = parseInt(prompt("How many charcters do you want in your password? Choose between 8 and 128"));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  //User input
+  if (!enter) {
+    alert("This needs a value between 8 and 128 characters");
+  }
+  else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("Must have characters between 8 and 128 characters"));
+  }
+  else {
+    confirmNumber = confirm("Will this contain numbers");
+    confirmCharacter = confirm("Will this contain letters?");
+    confirmLowercase = confirm("Will this contain Lowercase letters?");
+    confirmUppercase = confirm("Will this contain Uppercase letters?");
+  };
+  //All negative selections
+  if (!confirmCharacter && !confirmLowercase && !confirmNumber && !confirmUppercase) {
+    choices = alert("You must choose at least one!")
+  }
+  //One positive selection
+  else if (confirmCharacter) {
+    choices = special;
+  }
+  else if (confirmUppercase) {
+    choices = space.concat(letterUpper);
+  }
+  else if (confirmNumber) {
+    choices = number;
+  }
+  else if (confirmLowercase) {
+    choices = letter;
+  }
+  //Two positive selections
+  else if (confirmCharacter && confirmNumber) {
+    choices = special.concat(number);
+  }
+  else if (confirmCharacter && confirmUppercase) {
+    choices = special.concat(letterUpper);
+  }
+  else if (confirmCharacter && confirmLowercase) {
+    choices = special.concat(letter)
+  }
+  else if (confirmNumber && confirmLowercase) {
+    choices = number.concat(letter);
+  }
+  else if (confirmNumber && confirmUppercase) {
+    choices = number.concat(letterUpper)
+  }
+  else if (confirmLowercase && confirmUppercase) {
+    letter.concat(letterUpper)
+  }
 
 
 
